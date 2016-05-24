2a35d3703e67ae50a80ce25ad813a9cf0f8ffb5f
YOUR FULL NAME <Ho.luc13@gmail.com>
2016-05-23 17:31:55 -0700

    changed port on client server

--- client-server.js
+++ client-server.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const app = express();
+let port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/build'));
-app.listen(8080, () => console.log('server started on 8080.'));
+app.listen(port, () => console.log('server started on 8080.'));
