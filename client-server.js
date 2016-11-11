47f8ab12e08b33f59d251ed6f7fa7c132f0c2f4f
YOUR FULL NAME <Ho.luc13@gmail.com>
2016-11-11 15:08:40 -0800

    added proxy server. GET retrieves 12k lined object of stats'

--- client-server.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const express = require('express');
-const app = express();
-
-app.use(express.static(__dirname + '/build'));
-app.listen(8080, () => console.log('server started on 8080.'));
